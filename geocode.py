2913e969451433b10b72579174d5146c42b72272
zachariazh <saku.saarimaa@gmail.com>
2020-12-09 17:45:30 +0200

    update

--- geocode.py
+++ geocode.py
@@ -19,5 +19,7 @@ data = pd.read_csv(fp, sep=';')
 # Import the geocoding tool
 from geopandas.tools import geocode
 
-# Geocode addresses using Nominatim. Remember to provide a custom "application name" in the user_agent parameter!
-geo = geocode(data['addr'], provider='nominatim', user_agent='qmslup_ss', timeout=4)
+# Geocode addresses using Nominatim. Remember to provide a custom
+# "application name" in the user_agent parameter!
+geo = geocode(data['addr'], provider='nominatim'
+              , user_agent='qmslup_ss', timeout=4)
